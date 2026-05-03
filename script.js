4ab6a78bd4fe52621c740a3acaca0b44901711c7
赵俊飞 <1505811266@qq.com>
2026-05-03 08:40:56 +0800

    初始化

---
+++ script.js
@@ -0,0 +1,28 @@
+
+(function () {
+    window.shark_cloud_plugin_scripts_config = 
+    [
+     {
+         name: '五庄观仙果盛会活动',
+         actions: [
+             {
+                 name: '领取每日封神气韵',
+                 request_url: 'exchange',
+                 data: {
+                     id: 4001,
+                     num: 1,
+                     act: 2406002,
+                     uinY: 'player.uin',
+                     uIdx: 'player.uin',
+                     success_message: '领取每日免费气韵成功, 获得封神气韵x20',
+                     hasPackageReturn: true,
+                 }
+             }
+         ]
+     }
+ 
+ ];
+ })()
+ 
+ // https://gitee.com/jflyhak/shark_plugins/raw/develop/动态脚本/src/script.js
+ 
